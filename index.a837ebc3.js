19a9dee2f7029fbf71b40b3686f494a6f6b5163f
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-24 17:32:08 +0200

    make build

---
+++ index.a837ebc3.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("div"),e=document.querySelector("img"),o=t.offsetHeight,f=t.offsetWidth,s=e.offsetHeight,c=e.offsetWidth;e.style.position="absolute",e.style.top="".concat((o-s)/2,"px"),e.style.left="".concat((f-c)/2,"px");
+//# sourceMappingURL=index.a837ebc3.js.map
