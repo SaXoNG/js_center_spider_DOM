8fbd724bff5a70a34c7180a9fedfb24e0e88ea58
Vasyl <priymak17ukrvaha@gmail.com>
2025-02-24 16:45:21 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write code here
+const container = document.querySelector('div');
+const spider = document.querySelector('img');
+
+container.style.display = 'flex';
+container.style.justifyContent = 'center';
+container.style.alignItems = 'center';
+spider.style.position = 'relative';
